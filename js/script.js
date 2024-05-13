16c6269dc0910877b5f56efd8bce001e36c163d2
rafaeljuniora <160675357+rafaeljuniora@users.noreply.github.com>
2024-05-13 00:51:02 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,105 @@
+function scrolllor(){
+    var left=document.querySelector(".scroll-images-orquideas");
+    left.scrollBy(350, 0);
+}
+function scrollror(){
+    var right=document.querySelector(".scroll-images-orquideas");
+    right.scrollBy(-350, 0);
+}
+function scrolllbu(){
+    var left=document.querySelector(".scroll-images-buques");
+    left.scrollBy(350, 0);
+}
+function scrollrbu(){
+    var right=document.querySelector(".scroll-images-buques");
+    right.scrollBy(-350, 0);
+}
+function scrolllflo(){
+    var left=document.querySelector(".scroll-images-flornaturais");
+    left.scrollBy(350, 0);
+}
+function scrollrflo(){
+    var right=document.querySelector(".scroll-images-flornaturais");
+    right.scrollBy(-350, 0);
+}
+function scrolllca(){
+    var left=document.querySelector(".scroll-images-canecas");
+    left.scrollBy(350, 0);
+}
+function scrollrca(){
+    var right=document.querySelector(".scroll-images-canecas");
+    right.scrollBy(-350, 0);
+}
+function scrolllcho(){
+    var left=document.querySelector(".scroll-images-chocolates");
+    left.scrollBy(350, 0);
+}
+function scrollrcho(){
+    var right=document.querySelector(".scroll-images-chocolates");
+    right.scrollBy(-350, 0);
+}
+function scrollljar(){
+    var left=document.querySelector(".scroll-images-jardinagem");
+    left.scrollBy(350, 0);
+}
+function scrollrjar(){
+    var right=document.querySelector(".scroll-images-jardinagem");
+    right.scrollBy(-350, 0);
+}
+function scrolllpe(){
+    var left=document.querySelector(".scroll-images-pelucias");
+    left.scrollBy(350, 0);
+}
+function scrollrpe(){
+    var right=document.querySelector(".scroll-images-pelucias");
+    right.scrollBy(-350, 0);
+}
+function scrolllbe(){
+    var left=document.querySelector(".scroll-images-bebidas");
+    left.scrollBy(350, 0);
+}
+function scrollrbe(){
+    var right=document.querySelector(".scroll-images-bebidas");
+    right.scrollBy(-350, 0);
+}
+function scrolllcac(){
+    var left=document.querySelector(".scroll-images-cachepos");
+    left.scrollBy(350, 0);
+}
+function scrollrcac(){
+    var right=document.querySelector(".scroll-images-cachepos");
+    right.scrollBy(-350, 0);
+}
+function scrolllkit(){
+    var left=document.querySelector(".scroll-images-maternidade");
+    left.scrollBy(350, 0);
+}
+function scrollrkit(){
+    var right=document.querySelector(".scroll-images-maternidade");
+    right.scrollBy(-350, 0);
+}
+function scrolllvel(){
+    var left=document.querySelector(".scroll-images-velorio");
+    left.scrollBy(350, 0);
+}
+function scrollrvel(){
+    var right=document.querySelector(".scroll-images-velorio");
+    right.scrollBy(-350, 0);
+}
+function scrolllces(){
+    var left=document.querySelector(".scroll-images-cestas");
+    left.scrollBy(350, 0);
+}
+function scrollrces(){
+    var right=document.querySelector(".scroll-images-cestas");
+    right.scrollBy(-350, 0);
+}
+function scrollladu(){
+    var left=document.querySelector(".scroll-images-adubos");
+    left.scrollBy(350, 0);
+}
+function scrollradu(){
+    var right=document.querySelector(".scroll-images-adubos");
+    right.scrollBy(-350, 0);
+}
+
